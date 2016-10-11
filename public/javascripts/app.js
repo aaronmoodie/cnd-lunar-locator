beb7e85f65fcb05cb475352f2417cea70a1abed0
Aaron Moodie <hello@aaronmoodie.com>
2016-10-11 20:12:00 +1100

    setup initial methods for backbone app

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,1 +1,59 @@
-console.log("running!");
+/*
+  Lunar Locator app
+*/
+
+
+// init app
+var app = app || {};
+
+// Models & Collections
+// --------------------------------------------
+
+// Vehicle model
+app.VehicleItem = Backbone.Model.extend({
+
+});
+
+// Vehicle collection
+app.VehicleList = Backbone.Collection.extend({
+  model: VehicleItem
+});
+
+
+// Views
+// --------------------------------------------
+
+// Vehicle item view
+app.VehicleItemView = Backbone.View.extend({
+  tagName: 'li'
+});
+
+// Vehicle detail view
+app.VehicleDetailView = Backbone.View.extend({
+
+});
+
+// Vehicle collection view
+app.VehicleListView = Backbone.View.extend({
+  tagName: 'ul'
+});
+
+
+
+// Router & App setup
+// --------------------------------------------
+
+// App router
+app.Router = Backbone.Router.extend({
+
+});
+
+// Main app view
+app.AppView = Backbone.View.extend({
+
+});
+
+
+$(function(){
+    // new app.AppView();
+});
